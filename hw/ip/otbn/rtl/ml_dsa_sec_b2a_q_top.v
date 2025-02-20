82a9d874704b763e383f1e11ac3931c4f73b8d58
h-filali <h.filali98@gmail.com>
2025-02-20 15:45:00 +0000

    [otbn/rtl] Add some more signals to B2A top level

--- hw/ip/otbn/rtl/ml_dsa_sec_b2a_q_top.v
+++ hw/ip/otbn/rtl/ml_dsa_sec_b2a_q_top.v
@@ -2,35 +2,35 @@ module ml_dsa_sec_b2a_q_top (
   input wire  CLK100MHZ,
   input wire  ck_rst,
 
+// ready and valid signals
   inout wire [3:0] qspi_dq,
 
-  inout wire [3:0] eth_rxd,
-  inout wire [3:0] eth_txd,
+// z
+  output wire ck_io0,
+  output wire ck_io1,
+  output wire ck_io2,
+  output wire ck_io3,
+  output wire ck_io4,
+  output wire ck_io5,
+  output wire ck_io6,
+  output wire ck_io7,
+  output wire ck_io8,
+  output wire ck_io9,
+  output wire ck_io10,
+  output wire ck_io11,
+  output wire ck_io12,
+  output wire ck_io13,
+  output wire ck_io26,
+  output wire ck_io27,
+  output wire ck_io28,
+  output wire ck_io29,
+  output wire ck_io30,
+  output wire ck_io31,
+  output wire ck_io32,
+  output wire ck_io33,
+  output wire ck_io34,
 
-  input wire ck_io0,
-  input wire ck_io1,
-  input wire ck_io2,
-  input wire ck_io3,
-  input wire ck_io4,
-  input wire ck_io5,
-  input wire ck_io6,
-  input wire ck_io7,
-  input wire ck_io8,
-  input wire ck_io9,
-  input wire ck_io10,
-  input wire ck_io11,
-  input wire ck_io12,
-  input wire ck_io13,
-
-  input wire ck_io26,
-  input wire ck_io27,
-  input wire ck_io28,
-  input wire ck_io29,
-  input wire ck_io30,
-  input wire ck_io31,
-  input wire ck_io32,
-  input wire ck_io33,
-  input wire ck_io34,
+// x0
   input wire ck_io35,
   input wire ck_io36,
   input wire ck_io37,
@@ -38,31 +38,65 @@ module ml_dsa_sec_b2a_q_top (
   input wire ck_io39,
   input wire ck_io40,
   input wire ck_io41,
-
   inout wire [7:0] ja,
   inout wire [7:0] jb,
-  inout wire [7:0] jc,
-  inout wire [7:0] jd
+  
+//  x1
+  input wire ck_a0,
+  input wire ck_a1,
+  input wire ck_a2,
+  input wire ck_a3,
+  input wire ck_a4,
+  input wire ck_a5,
+  input wire vaux12_p,
+  input wire vaux12_n,
+  input wire vaux13_p,
+  input wire vaux13_n,
+  input wire vaux14_p,
+  input wire vaux14_n,
+  input wire ck_a6,
+  input wire ck_a7,
+  input wire ck_a8,
+  input wire ck_a9,
+  input wire ck_a10,
+  input wire ck_a11,
+  input wire ck_miso,
+  input wire ck_mosi,
+  input wire ck_sck,
+  input wire ck_ss,
+  input wire ck_scl,
+  input wire ck_sda,
+
+// gamma
+  input wire [7:0] jc,
+  input wire [3:0] eth_rxd,
+  input wire [3:0] eth_txd,
+
+// r
+  input wire [7:0] jd
  );
 
   localparam ShareWidth = 23;
   localparam RandWidth  = 80;
   localparam Modulus    = 8380417;
-  localparam Mask       = 23'h12C001;
 
-  wire [ShareWidth-1:0] z, z0, z1, x0, x1, gamma;
+  wire [ShareWidth-1:0] z, z0, z1, x0, x1;
+  wire [ShareWidth:0] gamma;
   wire [RandWidth-1:0] r;
 
   assign z = z0 ^ z1;
-  assign x1 = x0 ^ Mask;
+  assign x1 = {ck_a0, ck_a1, ck_a2, ck_a3, ck_a4, ck_a5,
+               vaux12_p, vaux12_n, vaux13_p, vaux13_n, vaux14_p, vaux14_n,
+               ck_a6, ck_a7, ck_a8, ck_a9, ck_a10, ck_a11, ck_miso, ck_mosi,
+               ck_sck, ck_ss, ck_scl, ck_sda};
   assign x0 = {ck_io35, ck_io36, ck_io37, ck_io38, ck_io39, ck_io40, ck_io41, ja, jb};
-  assign gamma = {jc[6:0], 2{jc}};
+  assign gamma = {jc, jc, eth_rxd, eth_txd};
   assign r = {10{jd}};
 
   ml_dsa_sec_b2a_q #(
-    .ShareWidth,
-    .RandWidth,
-    .Modulus
+    .ShareWidth(ShareWidth),
+    .RandWidth(RandWidth),
+    .Modulus(Modulus)
   ) i_ml_dsa_sec_b2a_q (
     .clk_i(CLK100MHZ),
     .rst_ni(ck_rst),
@@ -72,7 +106,7 @@ module ml_dsa_sec_b2a_q_top (
     .ready_i(qspi_dq[3]),
     .x0_i(x0),
     .x1_i(x1),
-    .gamma_i(gamma),
+    .gamma_i(gamma[ShareWidth-1:0]),
     .r_i(r),
     .z0_o(z0),
     .z1_o(z1)
